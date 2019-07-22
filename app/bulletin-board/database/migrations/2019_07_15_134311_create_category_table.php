08b0546ae1066a1422cf431bc67c6dc776b432ab
channel-4 <ch4.ito.y@gmail.com>
2019-07-23 06:43:04 +0900

    [add]アプリ本体(bulletin-board)を追加

---
+++ app/bulletin-board/database/migrations/2019_07_15_134311_create_category_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('category', function(Blueprint $table) {
+            $table->smallIncrements('id');
+            $table->string('name');
+            $table->string('handle_name_template');
+            $table->dateTime('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reply');
+        Schema::dropIfExists('thread');
+        Schema::dropIfExists('category');
+    }
+}
